04e10b9b3c24a400c2dab793eb17ed2efc354baf
Taylor Alan <tay10alan60@gmail.com>
2023-01-10 10:51:09 -0800

    controller
    Co-authored-by: jemzir <jemzir@users.noreply.github.com>

---
+++ src/server/controllers/loginController.js
@@ -0,0 +1,19 @@
+const db = require('./models');
+
+const controller = {};
+
+controller.checkProfile = async (req, res, next) => {
+    //initial query of database
+    //if user previous login 
+    //go to song controller with user table info
+
+    //else create new user
+    //create table for user songs played
+    // create table for blacklist
+
+    //return error
+
+
+}
+
+module.exports = controller;
