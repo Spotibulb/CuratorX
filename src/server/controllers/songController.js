58715b8716908b8f4a8763d4321b2f0773b2b7aa
Taylor Alan <tay10alan60@gmail.com>
2023-01-10 10:57:27 -0800

    jnt songcontroller

---
+++ src/server/controllers/songController.js
@@ -0,0 +1,22 @@
+const db = require('./models');
+
+const songController = {};
+
+songController.songQuery = async (req, res, next) => {
+  /**
+   * 
+   * query the api and get a search result
+   *  compare that against the user's song table database
+   *    if song === the query
+   * continue down the list
+   *    once we find a falsey result, we store the result in res.locals
+   *    push that song into the userSong table database
+   *  return next()
+   * 
+   * error handling return (error)
+   */
+
+
+}
+
+module.exports = songController;
