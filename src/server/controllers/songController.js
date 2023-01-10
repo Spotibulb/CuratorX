24e632855a8e87016fa4d64a77c8d3a20b4dfda5
Taylor Alan <tay10alan60@gmail.com>
2023-01-10 10:59:14 -0800

    refactored pseudocode of song controller

--- src/server/controllers/songController.js
+++ src/server/controllers/songController.js
@@ -7,7 +7,7 @@ songController.songQuery = async (req, res, next) => {
    * 
    * query the api and get a search result
    *  compare that against the user's song table database
-   *    if song === the query
+   *    if song === the query  && not found in blacklist
    * continue down the list
    *    once we find a falsey result, we store the result in res.locals
    *    push that song into the userSong table database
